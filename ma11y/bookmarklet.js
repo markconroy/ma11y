397cb6533296bb73fcd0b24f1b493665169d91c5
Mark Conroy <mark@mark.ie>
2024-03-12 19:48:33 +0000

    adds comments to bookmarklet script

--- ma11y/bookmarklet.js
+++ ma11y/bookmarklet.js
@@ -1,7 +1,15 @@
-const ma11yBmScript = document.createElement("script");
-ma11yBmScript.src = "https://ma11y.mark.ie/ma11y/script.js";
-const ma11yBmBody = document.querySelector("body");
-ma11yBmBody.appendChild(ma11yBmScript);
+// A bookmarklet to load the ma11y tool.
+// We are keeping this separate from the main script so that the main script
+// can be updated independently of the bookmarklet.
+
+const ma11yBookmarkletScript = document.createElement("script");
+ma11yBookmarkletScript.src = "https://ma11y.mark.ie/ma11y/script.js";
+
+const ma11yBookmarkletBody = document.querySelector("body");
+ma11yBookmarkletBody.appendChild(ma11yBookmarkletScript);
+
+// Add a small delay to ensure the script has loaded before we try to activate
+// it.
 setTimeout(() => {
-  document.querySelector("#ma11y-tools").setAttribute('data-active', 'true');
+  document.querySelector("#ma11y-tools").setAttribute("data-active", "true");
 }, 250);
